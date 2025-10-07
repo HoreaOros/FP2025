42dc730b88c1d23e3c80575d8dd31ec03f8688d2
Horea Oros <horea.oros@gmail.com>
2025-10-07 11:59:01 +0300

    add solution Collatz problem

--- FP2025/Program.cs
+++ FP2025/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Kollatz sequence
+
+// This program calculates
+// the Collatz sequence for a given positive integer.
+
+
+int maxLen = 0;
+int maxNo = 0;
+
+for (int n = 1; n <= 1000; n++)
+{
+    int len = K(n);
+    
+    if(len >  maxLen )
+    {
+        maxLen = len;
+        maxNo = n;
+    }
+}
+
+Console.WriteLine($"Cea mai lunga secventa are lungimea {maxLen} si se obtine pentru numarul {maxNo}");
+
+int K(int n)
+{
+    int length = 1;
+    Console.Write($"{n} ");
+    while (n != 1)
+    {
+        length++;
+        if (n % 2 == 0) 
+            n = n / 2; 
+        else 
+            n = 3 * n + 1;
+        Console.Write($"{n} ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+
+
+    return length;
+}
+
+// side effect
+
+
+
+
+
+
